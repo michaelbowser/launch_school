8a981c4bf9da8272d58309d3b3f07f03c459806d
michaelbowser <michael@michaelbowser.org>
2026-02-13 11:16:58 -0600

    updated to float

--- intro_to_python/sum_numbers.py
+++ intro_to_python/sum_numbers.py
@@ -1,5 +1,5 @@
 number1 = input('Enter the first number: ')
 number2 = input('Enter the second number')
-sum = number1 + number2    
+sum = float(number1) + float(number2)    
 
 print(f'The numbers {number1} and {number2} 'f'add to {sum}')
