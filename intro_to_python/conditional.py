135ba181e8bb5cb73875f4abb3aee549d9ebe436
michaelbowser <michael@michaelbowser.org>
2026-02-20 09:47:17 -0600

    more practice

---
+++ intro_to_python/conditional.py
@@ -0,0 +1,9 @@
+value = int(input('Enter a number: '))
+
+if value == 3:
+    print('value is 3')
+elif value == 4:
+        pass
+
+else:
+    print('value is NOT 3')
---
+++ intro_to_python/say2.py
@@ -0,0 +1,8 @@
+def say(text):
+    print(text)
+
+print('hello')
+print('hi')
+print('how do you do')
+print('Quite all right')
+
