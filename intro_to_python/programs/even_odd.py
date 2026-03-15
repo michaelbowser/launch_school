99f3846ce9f6cffe00cb82a11af4a4abce4b1e5c
michaelbowser <michael@michaelbowser.org>
2026-03-15 12:31:40 -0500

    more loops work

---
+++ intro_to_python/programs/even_odd.py
@@ -0,0 +1,4 @@
+my_list = [6, 3, 0, 11, 20, 17]
+
+while index < len(my_list):
+    even = index
---
+++ intro_to_python/programs/my_list.py
@@ -0,0 +1,11 @@
+my_list = [6, 3, 0, 11, 20, 4, 17]
+"""
+index = 0
+
+while index < len(my_list):
+    number = my_list[index]
+    print(number)
+    index += 1
+"""
+for number in my_list:
+    print(number)
