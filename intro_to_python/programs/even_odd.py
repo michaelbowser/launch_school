d0e7d1335f911a0e23a2e77f69c665365a1b122b
michaelbowser <michael@michaelbowser.org>
2026-03-16 13:53:19 -0500

    adding using collections practice

--- intro_to_python/programs/even_odd.py
+++ intro_to_python/programs/even_odd.py
@@ -1,4 +1,14 @@
 my_list = [6, 3, 0, 11, 20, 17]
-
+"""
+# Even function
+index = 0
 while index < len(my_list):
-    even = index
+    number = my_list[index]
+    if number % 2 == 0:
+        print(number)
+
+    index += 1
+"""    
+for number in my_list:
+    if number % 2 != 0:
+        print(number)
