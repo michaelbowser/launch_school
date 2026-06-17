60bd4b2e49070294779cbd47f3be4d7bbce7ff72
michaelbowser <michael@michaelbowser.org>
2026-06-17 12:11:15 -0500

    added error checking

--- py_101/loan_calculator/loan_calculator.py
+++ py_101/loan_calculator/loan_calculator.py
@@ -1,21 +1,44 @@
-print("Welcome to The Loan Calculator!") 
-print("Please enter the dollar amount of the loan")
+def prompt(message):
+    print(f"==> {message}")
 
-loan_amount_entry = input ()
-loan_amount = int(loan_amount_entry)
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+            return True
 
-print(f' Your loan amount is ${loan_amount}')
-print("Now, please enter your APR. You can enter as a decimal,such as 12.5")
+    return False 
 
+prompt("Welcome to The Loan Calculator!")
+
+prompt("Please enter the amount of the loan")
+loan_entry = input()
+
+while invalid_number(loan_entry):
+    prompt("Hmm.. that doesn't look like a valid number. Please try again.")
+    loan_entry = input()
+
+loan_amount = int(loan_entry)
+
+ # print(type(loan_amount))
+
+prompt(f' Your loan amount is ${loan_amount}')
+
+prompt("Now, please enter your APR. You can enter as a decimal,such as 12.5")
 apr_entry = input()
+
+while invalid_number(apr_entry):
+    prompt("Hmm.. that doesn't look like a mumber. Please try again.")
+    apr_entry = input()
+
 apr = (float(apr_entry)/ 100)
 
 monthly_interest_rate = (apr/12)
 
 
-print(f' Your interest rate is {apr}%')
+prompt(f' Your interest rate is {apr}%')
 
-print (f' Finally, How many years will your loan be for?')
+prompt(f' Finally, How many years will your loan be for?')
 
 loan_duration_entry = input()
 loan_duration = (int(loan_duration_entry)*12)
@@ -23,4 +46,4 @@ loan_duration = (int(loan_duration_entry)*12)
 monthly_payment = loan_amount * (monthly_interest_rate/(1 -(1 +
 monthly_interest_rate) ** (-loan_duration)))
     
-print(f' Your payment will be ${monthly_payment} per month.')
+prompt(f' Your payment will be ${monthly_payment} per month.')
